ba9d8cc5498923d476e85f5b6d217786636bb62b
Ali Pourgheysari <a.geysar@gmail.com>
2023-05-12 15:20:40 +0430

    added comments

--- FrozenLake.py
+++ FrozenLake.py
@@ -24,7 +24,7 @@ QTable = np.zeros((size*size, 4))
 # create Rewards
 Rewards = np.zeros(size*size)
 
-# set the reward of the goal and the hole
+# set the reward of each state
 for i, rowVal in enumerate(env.unwrapped.spec.kwargs['desc']):
     for j, colVal in enumerate(rowVal):
         coord = i*size+j
@@ -35,6 +35,8 @@ for i, rowVal in enumerate(env.unwrapped.spec.kwargs['desc']):
         else:
             Rewards[coord] = state_reward
 
+# calculate if the agent can move to the next state
+
 
 def CanMoveToNextState(observation, action):
     if env.unwrapped.P[observation][action][0][1] == observation:
@@ -42,12 +44,17 @@ def CanMoveToNextState(observation, action):
     else:
         return True
 
+# choose action
+
 
 def choose_action():
     actions_index = []
+
+    # Get the index and value of each action
     for i in range(4):
         actions_index.append((QTable[observation][i], i))
 
+    # Sort the actions by their values
     sorted_list = sorted(actions_index, key=lambda x: x[0], reverse=True)
 
     # Initialize a dictionary to group elements by their first values
@@ -67,12 +74,13 @@ def choose_action():
     # Flatten the groups back into a single list
     shuffled_list = [elem for group in groups.values() for elem in group]
 
-    # shuffled_list[0][1] > 0 or
+    # Choose the action with the highest value with probability (1 - epsilon) (epsilon-greedy)
     if np.random.uniform(0, 1) < (1 - epsilon):
         action = shuffled_list[0][1]
     else:
         action = env.action_space.sample()
 
+    # If the action is not valid, choose the next action with the highest value
     i = 1
     while True:
         if CanMoveToNextState(observation, action):
@@ -80,30 +88,39 @@ def choose_action():
         action = sorted_list[i][1]
         i += 1
 
+    # Return the chosen action
     return action
 
-
+# Q-learning algorithm
 def Q_learning():
 
     while True:
         action = choose_action()
         new_observation, reward, terminated, truncated, info = env.step(action)
 
+        # get the maximum value of the next state
         maximum_list = [value for i, value in enumerate(
             QTable[new_observation]) if CanMoveToNextState(new_observation, i)]
 
+        # if the maximum list is empty, it means that the agent is in a hole
+        # so we add the hole reward to the maximum list and decrease the hole reward
+        # so next time the agent will not get stock in the hole
         if not maximum_list:
             global hole_reward
             maximum_list.append(hole_reward)
             hole_reward -= size
 
+        # update the QTable
         global observation
         QTable[observation, action] = QTable[observation, action] + alpha * \
             (Rewards[new_observation] + gamma *
              np.max(maximum_list) - QTable[observation, action])
 
+        # update the Rewards
         Rewards[observation] = np.average(QTable[observation])
 
+        # print the actions of the old state, the actions of the new state, 
+        # the action chosen, the reward of the old state and the reward of the new state
         print(QTable[observation], QTable[new_observation],
               action, Rewards[observation], Rewards[new_observation])
         observation = new_observation
@@ -112,10 +129,11 @@ def Q_learning():
             observation, info = env.reset()
             break
 
-
+# learning phase
 for _ in range(max_iter_number):
     Q_learning()
 
+# test phase
 while True:
     action = np.argmax(QTable[observation])
     observation, reward, terminated, truncated, info = env.step(action)
